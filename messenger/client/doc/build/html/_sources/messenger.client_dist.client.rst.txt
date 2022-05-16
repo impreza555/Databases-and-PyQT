8a724faafc8064add6fafeb264524525b35cf2dc
impreza555 <alex.erjomin@outlook.com>
2022-05-16 14:57:00 +0300

    Сделал Д.З.7.

---
+++ messenger/client/doc/build/html/_sources/messenger.client_dist.client.rst.txt
@@ -0,0 +1,69 @@
+client - ядро приложения, а так же графический интерфейс.
+=========================================================
+
+Submodules
+----------
+
+add\_contact\_dialog\_ui.py
+---------------------------
+
+.. automodule:: messenger.client_dist.client.add_contact_dialog_ui
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+client\_db.py - модуль для работы с базой данных.
+-------------------------------------------------
+
+.. automodule:: messenger.client_dist.client.client_db
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+client\_gui.py - модуль графического интерфейса.
+------------------------------------------------
+
+.. automodule:: messenger.client_dist.client.client_gui
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+client\_main.py - модуль ядра приложения.
+-----------------------------------------
+
+.. automodule:: messenger.client_dist.client.client_main
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+client\_main\_ui.py
+-------------------
+
+.. automodule:: messenger.client_dist.client.client_main_ui
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+del\_contact\_dialog\_ui.py
+---------------------------
+
+.. automodule:: messenger.client_dist.client.del_contact_dialog_ui
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+start\_dialog\_ui.py
+--------------------
+
+.. automodule:: messenger.client_dist.client.start_dialog_ui
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: messenger.client_dist.client
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- messenger/launcher.py
+++ messenger/launcher.py
@@ -2,25 +2,35 @@ import os
 from subprocess import Popen, CREATE_NEW_CONSOLE
 
 process = []
-client_path = os.path.abspath(os.path.join(os.path.dirname(__file__), './client_dist/'))
-server_path = os.path.abspath(os.path.join(os.path.dirname(__file__), './server_dist/'))
+client_path = os.path.abspath(os.path.join(os.path.dirname(__file__), './client/'))
+server_path = os.path.abspath(os.path.join(os.path.dirname(__file__), './server/'))
 
-while True:
-    action = input('Выберите действие: q - выход , s - запустить сервер, '
-                   'k - запустить клиенты x - закрыть все окна:')
-    if action == 'q':
-        break
-    elif action == 's':
-        process.append(Popen('python run_server.py', cwd=server_path,
-                             creationflags=CREATE_NEW_CONSOLE))
-    elif action == 'k':
-        print('Убедитесь, что на сервере зарегистрировано необходимое'
-              ' количество клиентов с паролем 123456.')
-        print('Первый запуск может быть достаточно долгим из-за генерации ключей!')
-        clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
-        for i in range(clients_count):
-            process.append(Popen(f'python run_client.py -n test{i + 1} -p 123456',
-                                 cwd=client_path, creationflags=CREATE_NEW_CONSOLE))
-    elif action == 'x':
-        while process:
-            process.pop().kill()
+
+def start_applications():
+    """
+    Функция запуска приложений из командной строки.
+    :return: None
+    """
+    while True:
+        action = input('Выберите действие: q - выход , s - запустить сервер, '
+                       'k - запустить клиенты x - закрыть все окна:')
+        if action == 'q':
+            break
+        elif action == 's':
+            process.append(Popen('python run_server.py', cwd=server_path,
+                                 creationflags=CREATE_NEW_CONSOLE))
+        elif action == 'k':
+            print('Убедитесь, что на сервере зарегистрировано необходимое'
+                  ' количество клиентов с паролем 123456.')
+            print('Первый запуск может быть достаточно долгим из-за генерации ключей!')
+            clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
+            for i in range(clients_count):
+                process.append(Popen(f'python run_client.py -n test{i + 1} -p 123456',
+                                     cwd=client_path, creationflags=CREATE_NEW_CONSOLE))
+        elif action == 'x':
+            while process:
+                process.pop().kill()
+
+
+if __name__ == '__main__':
+    start_applications()
